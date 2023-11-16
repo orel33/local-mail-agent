0cc1d33075eda0c9d87fb8b563531db5cd8a4927
Aurelien Esnard <aurelien.esnard@u-bordeaux.fr>
2023-11-16 16:36:03 +0100

    minor

--- run-cremi.sh
+++ run-cremi.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-[ -z "$TMPDIR" ] && TMPDIR=/tmp # or /local ?
-[ -z "$USER" ] && USER=$(whoami)
-CMD='podman'
-OPT="--rm -it --hostname=pouet.com"
-STORAGE="--storage-driver=overlay --root=$TMPDIR/$USER/containers"
-EXPOSE="-p 10110:110 -p 10995:995 -p 10025:25 -p 10465:465"
+# alias podman="podman --root=$TMPDIR/containers --storage-driver=overlay"
+# alias docker=podman
 
-$CMD run $OPT $EXPOSE $STORAGE orel33/local-mail-agent
+# alias podman-compose="podman-compose --podman-args="--root=$TMPDIR/containers --storage-driver=overlay"
+# alias docker-compose="podman-compose"
+
+# [ -z "$TMPDIR" ] && TMPDIR=/tmp/$USER
+# [ -z "$TMPDIR" ] && TMPDIR=/local/$USER 
+# [ -z "$USER" ] && USER=$(whoami)
+
+OPT="--rm -it --hostname=pouet.com"
+STORAGE="--storage-driver=overlay --root=$TMPDIR/containers"
+EXPOSE="-p 10110:110 -p 10995:995 -p 10025:25 -p 10465:465"
+IMG="registry.u-bordeaux.fr/l2info/local-mail-agent"
+
+podman run $OPT $EXPOSE $STORAGE $IMG
 
